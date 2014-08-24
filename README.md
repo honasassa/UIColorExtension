eea7a15e236287ad210fb3871dc99270427aaa16
go kikuta <go.kikt@gmail.com>
2014-08-24 18:07:44 +0900

    refactor methods and add sample

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ UIColor(hexa: 0xFF0000FF) // 1.0, 0.0, 0.0, 1.0 RGBA
 UIColor(red255: 255, green255: 0, blue255: 0, alpha255: 0) // 1.0, 0.0, 0.0, 0.0 RGBA
 UIColor.colorHexa(0xFF00FFFF) // 1.0, 0.0, 0.0, 0.0 RGBA
 UIColor.colorHex(0xFF00FF) // 1.0, 0.0, 0.0, 0.0 RGB
+UIColor(hex: "FF0000") // 1.0, 0.0, 0.0 RGB
         
 UIColor.random(); // output random rgb color (alpha = 1.0)
 UIColor.randoma(); // output random rgba color
--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -33,44 +33,65 @@ extension UIColor {
         self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
     
-    class func color255(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 255.0) -> UIColor!
+    convenience init(hex: String)
     {
-        return UIColor(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha / 255.0)
+        var red:CGFloat   = 0.0
+        var green:CGFloat = 0.0
+        var blue:CGFloat  = 0.0
+        var alpha:CGFloat = 1.0
+        
+        let scanner = NSScanner.scannerWithString(hex)
+        var hexValue: UInt32 = 0
+        if scanner.scanHexInt(&hexValue) {
+            if countElements(hex) == 8 {
+                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                blue  = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
+                alpha = CGFloat((hexValue & 0x000000FF)) / 255.0
+            } else if countElements(hex) == 6 {
+                red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
+                blue  = CGFloat((hexValue & 0x0000FF)) / 255.0
+            }
+        }
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
     
+    class func color255(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 255.0) -> UIColor!
+    {
+        return UIColor(red255: red, green255: green, blue255: blue, alpha255: alpha)
+    }
+
     class func colorHex(hex: UInt32, alpha: CGFloat = 1.0) -> UIColor!
     {
-        let red:CGFloat   = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-        let green:CGFloat = CGFloat((hex & 0x00FF00) >> 8) / 255.0
-        let blue:CGFloat  = CGFloat((hex & 0x0000FF)) / 255.0
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return UIColor(hex: hex, alpha: alpha)
     }
     
     class func colorHexa(hexa: UInt32) -> UIColor!
     {
-        let red:CGFloat   = CGFloat((hexa & 0xFF000000) >> 24) / 255.0
-        let green:CGFloat = CGFloat((hexa & 0x00FF0000) >> 16) / 255.0
-        let blue:CGFloat  = CGFloat((hexa & 0x0000FF00) >> 8) / 255.0
-        let alpha:CGFloat = CGFloat((hexa & 0x000000FF)) / 255.0
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return UIColor(hexa: hexa)
+    }
+    
+    class func colorHex(hex: String) -> UIColor!
+    {
+        return UIColor(hex: hex)
     }
     
     class func random() -> UIColor!
     {
-        let r:CGFloat = CGFloat(arc4random() % UInt32(255));
-        let g:CGFloat = CGFloat(arc4random() % UInt32(255));
-        let b:CGFloat = CGFloat(arc4random() % UInt32(255));
-        return UIColor.color255(r, green: g, blue: b, alpha: 255.0)
+        let red:CGFloat   = CGFloat(arc4random() % UInt32(255));
+        let green:CGFloat = CGFloat(arc4random() % UInt32(255));
+        let blue:CGFloat  = CGFloat(arc4random() % UInt32(255));
+        return UIColor.color255(red, green: green, blue: blue, alpha: 255.0)
     }
     
     class func randoma() -> UIColor!
     {
-        let r:CGFloat = CGFloat(arc4random() % UInt32(255));
-        let g:CGFloat = CGFloat(arc4random() % UInt32(255));
-        let b:CGFloat = CGFloat(arc4random() % UInt32(255));
-        let a:CGFloat = CGFloat(arc4random() % UInt32(255));
-        return UIColor.color255(r, green: g, blue: b, alpha: a)
+        let red:CGFloat   = CGFloat(arc4random() % UInt32(255));
+        let green:CGFloat = CGFloat(arc4random() % UInt32(255));
+        let blue:CGFloat  = CGFloat(arc4random() % UInt32(255));
+        let alpha:CGFloat = CGFloat(arc4random() % UInt32(255));
+        return UIColor.color255(red, green: green, blue: blue, alpha: alpha)
     }
 }
